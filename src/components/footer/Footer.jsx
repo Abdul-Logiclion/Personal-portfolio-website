8cc9cf54cc84cd9f6afaa11a3d18b722e21689c3
Pawan Kumar <110078093+Pawan-Kumar03@users.noreply.github.com>
2023-08-12 19:00:12 +0500

    Fixed mobile size responsive design

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,6 +3,7 @@ import './footer.css'
 import {FaFacebookF} from 'react-icons/fa'
 import {FiInstagram} from 'react-icons/fi'
 import {RiMetaLine} from 'react-icons/ri'
+import {BsLinkedin} from 'react-icons/bs'
 
 const Footer = () =>{
     return(
@@ -23,6 +24,7 @@ const Footer = () =>{
                 <a href="https://web.facebook.com/pawan.toga.12?mibextid=ZbWKwL&_rdc=1&_rdr" target="_blank"><FaFacebookF/></a>
                 <a href="https://www.instagram.com/pawantoga/?igshid=MzNlNGNkZWQ4Mg%3D%3D"target="_blank"><FiInstagram/></a>
                 <a href="https://www.threads.net/@pawantoga"target="_blank"><RiMetaLine/></a>
+                <a href="https://www.linkedin.com/in/pawan-kumar-3b1869246/"target="_blank"><BsLinkedin/></a>
             </div>
             <div className="footer__copyright">
                 <small>&copy; Pawantoga. All Rights Reserved.</small>
